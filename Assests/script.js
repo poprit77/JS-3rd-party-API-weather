8d13c01179cbcf433cbab0c4bf84ab4a076cdfcf
unknown <peterataras@gmail.com>
2022-11-29 16:14:05 -0800

    Functioning API

--- Assests/script.js
+++ Assests/script.js
@@ -1,14 +1,84 @@
+
+// fetch('https://api.openweathermap.org/data/2.5/weather?lat=44.34&lon=10.99&appid=06bb58b25b45536bf564aaa75898fb91')
+//     .then(function (response) {
+//       if (response.ok) {
+//         console.log(response);
+//         response.json().then(function (data) {
+//           console.log(data);
+//         });
+//               } else {
+//         alert('Error: ' + response.statusText);
+//       }
+//     })
+//     .catch(function (error) {
+//       alert('Unable to connect to GitHub');
+//     });
+
 console.log('this');
+var userFormEl = document.querySelector('#user-form');
+var nameInputEl = document.querySelector('#username')
+
+var Location1 = nameInputEl.value;
 
 
-fetch("https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid=06bb58b25b45536bf564aaa75898fb91", {
-    // The browser fetches the resource from the remote server without first looking in the cache.
-    // The browser will then update the cache with the downloaded resource.
+var formSubmitHandler = function (event) {
+  event.preventDefault();
+  var username = nameInputEl.value.trim();
+  if (username) {
+    getUserRepos(username);
+    nameInputEl.value = '';
+  } else {
+    alert('Please enter a GitHub username');
+  }
+};
+var getUserRepos2 = function (lat ,lon) {
+  var apiUrl2 = 'https://api.openweathermap.org/data/2.5/weather?lat=' + lat + '&lon=' + lon + '&appid=06bb58b25b45536bf564aaa75898fb91';
 
-})
+  fetch(apiUrl2)
     .then(function (response) {
-        return response.json();
+      if (response.ok) {
+        console.log(response);
+        response.json().then(function (data) {
+          console.log(data);
+        });
+      } else {
+        alert('Error: ' + response.statusText);
+      }
     })
-    .then(function (data) {
-        console.log(data);
+    .catch(function (error) {
+      alert('Unable to connect to GitHub');
     });
+};
+
+var getUserRepos = function (user) {
+  var apiUrl = 'https://api.openweathermap.org/geo/1.0/direct?q={' + user + '}&appid=06bb58b25b45536bf564aaa75898fb91';
+
+  fetch(apiUrl)
+    .then(function (response) {
+      if (response.ok) {
+        console.log(response);
+        response.json().then(function (data) {
+          console.log(data);
+          for (var i = 0; i < data.length; i++) {
+            // console.log(data[i].lat);
+            var lat = data[i].lat;
+            var lon = data[i].lon;
+            // console.log(data[i].lon);
+            // displayRepos(data, user);
+
+            console.log(lat);
+            console.log(lon);
+            getUserRepos2(lat, lon);
+          }
+        });
+      } else {
+        alert('Error: ' + response.statusText);
+      }
+    })
+    .catch(function (error) {
+      alert('Unable to connect to GitHub');
+    });
+};
+// console.log(lat);
+// console.log(lon);
+userFormEl.addEventListener('submit', formSubmitHandler);
